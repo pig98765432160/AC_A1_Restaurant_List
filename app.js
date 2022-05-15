2ceda8c9383f73a40aa2e94dffd8ba7c5f81a337
Vivian <pig98765432160@gmail.com>
2022-05-15 17:30:16 +0800

    [Add] css、餐廳清單

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const restaurantList = require("./restaurant.json")
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
 
 // setting the route and corresponding response
-app.get('/', (req, res) => {
-  res.send(`This is my first Express Web App`)
+app.get('/', (req, res ) => {
+  res.render("index", { restaurants: restaurantList.results })
+})
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render("index", { restaurants: restaurants, keyword: keyword })
+})
+
+app.get("/restaurants/:restaurant_Id", (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_Id)  
+  res.render("show", { restaurant: restaurant })
 })
 
 // Listen the server when it started
